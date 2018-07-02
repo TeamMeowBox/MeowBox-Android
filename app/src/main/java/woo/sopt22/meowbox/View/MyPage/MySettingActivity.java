d19490f52ab2c01fa400f0751eebd2304f61fb68
baky94 <baky94@naver.com>
2018-07-02 17:30:30 +0900

    mypage 레이아웃과                                                                                            drawable 해상도별

--- app/src/main/java/woo/sopt22/meowbox/View/MyPage/MySettingActivity.java
+++ app/src/main/java/woo/sopt22/meowbox/View/MyPage/MySettingActivity.java
@@ -10,6 +10,7 @@ public class MySettingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_my_setting);
 
     }

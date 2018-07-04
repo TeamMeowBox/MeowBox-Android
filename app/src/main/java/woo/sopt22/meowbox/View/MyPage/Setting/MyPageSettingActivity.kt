75d9eb905ecd4b5e7d2ca19677e5d9aee640382c
baky94 <baky94@naver.com>
2018-07-04 12:28:46 +0900

    Merge branch 'develop' of ssh://github.com/TeamMeowBox/MeowBox-Android into develop
    # Conflicts:
    #	app/src/main/java/woo/sopt22/meowbox/View/MyPage/MyPageActivity.kt

--- app/src/main/java/woo/sopt22/meowbox/View/MyPage/Setting/MyPageSettingActivity.kt
+++ app/src/main/java/woo/sopt22/meowbox/View/MyPage/Setting/MyPageSettingActivity.kt
@@ -1,5 +1,7 @@
 package woo.sopt22.meowbox.View.MyPage.Setting
 
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableString
@@ -7,20 +9,35 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_my_page_setting.*
 import woo.sopt22.meowbox.R
 import android.text.style.UnderlineSpan
+import android.view.View
+import android.widget.ImageView
 
 
+class MyPageSettingActivity : AppCompatActivity(), View.OnClickListener{
+    override fun onClick(p0: View?) {
+        when(p0) {
+            my_page_setting_x_btn -> {
+                finish()
+            }
+        }
+    }
 
-class MyPageSettingActivity : AppCompatActivity() {
-
-    var tv1 = my_page_setting_disjoin as TextView
+    // var tv1 = my_page_setting_disjoin as TextView
+    lateinit var myPageSettingXBtn : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page_setting)
 
-        val content = SpannableString("Content")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            window.statusBarColor = Color.BLACK
+
+        val content = SpannableString("여기")
         content.setSpan(UnderlineSpan(), 0, content.length, 0)
-        tv1.setText(content)
+        my_page_setting_disjoin.setText(content)
+
+        myPageSettingXBtn = my_page_setting_x_btn as ImageView
+        my_page_setting_x_btn.setOnClickListener(this)
 
 
     }

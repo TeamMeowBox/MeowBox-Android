749d3c0d48d18d0041400c0557cbca2d11ebb9ef
Woo <jhsw0375@gmail.com>
2018-07-04 10:56:25 +0900

    MyPageSuggestActivity, MyPageSettingActivity 추가

---
+++ app/src/main/java/woo/sopt22/meowbox/View/MyPage/Suggest/MyPageSuggestActivity.kt
@@ -0,0 +1,13 @@
+package woo.sopt22.meowbox.View.MyPage.Suggest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import woo.sopt22.meowbox.R
+
+class MyPageSuggestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page_suggest)
+    }
+}

f25f33cdb3579a1b81ae238f4b7585f4711641c3
Woo <jhsw0375@gmail.com>
2018-07-04 11:37:34 +0900

    미유박스에 제안하기 페이지 구현 완료

--- app/src/main/java/woo/sopt22/meowbox/View/MyPage/Suggest/MyPageSuggestActivity.kt
+++ app/src/main/java/woo/sopt22/meowbox/View/MyPage/Suggest/MyPageSuggestActivity.kt
@@ -1,13 +1,67 @@
 package woo.sopt22.meowbox.View.MyPage.Suggest
 
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import kotlinx.android.synthetic.main.activity_my_page_suggest.*
 import woo.sopt22.meowbox.R
+import woo.sopt22.meowbox.View.MyPage.MyPageActivity
 
-class MyPageSuggestActivity : AppCompatActivity() {
+class MyPageSuggestActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        when(v!!){
+            suggest_btn->{
+                // 서버랑 통신
+            }
+            suggest_x_btn->{
+                finish()
+            }
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page_suggest)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            window.statusBarColor = Color.BLACK
+
+        suggest_btn.setOnClickListener(this)
+        suggest_x_btn.setOnClickListener(this)
+
+
+        // 제안하기
+        suggest_opinion.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        // 자세히 알려주세요.
+        suggest_detail_comment.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+
     }
 }

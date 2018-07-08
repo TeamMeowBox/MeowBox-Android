4a2542fd06cfce0781fc22d13f3a903c248f0fcc
Woo <jhsw0375@gmail.com>
2018-07-08 12:09:45 +0900

    MeowBoxDetail 주문하기 눌렀을 때, 분기 처리

--- app/src/main/java/woo/sopt22/meowbox/View/Home/MainActivity.kt
+++ app/src/main/java/woo/sopt22/meowbox/View/Home/MainActivity.kt
@@ -270,7 +270,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                         startActivity(intent)
                     }
                 }
-
             }
             R.id.reviewBtn -> {
                 startActivity(Intent(this, MeowBoxReviewActivity::class.java))
